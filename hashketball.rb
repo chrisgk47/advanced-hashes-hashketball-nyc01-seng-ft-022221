12f0008e1b5978ef68b378335bc08cd6cf8edbdd
Christopher Kim <chris.kim1990@gmail.com>
2021-02-15 19:30:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,8 +134,8 @@ def num_points_scored(name)
       players.each do |points|
         players[points][3]
       end
-      binding.pry
     end
+    binding.pry
     away.each do |players|
       players.each do |points|
         players[points][3]
