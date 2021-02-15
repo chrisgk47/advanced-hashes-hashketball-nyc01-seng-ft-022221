702d8bea5f5cbb04807e926e271e8a0542b4f0c3
Christopher Kim <chris.kim1990@gmail.com>
2021-02-15 20:08:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -151,7 +151,7 @@ end
 def team_colors(team)
   team_color = {}
   game_hash.each do |game, team_stats|
-    team_stats[:colors].each do |colors|
+    team_stats.each do |colors|
       binding.pry
 
 
